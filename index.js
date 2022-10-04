dd93c4820339911bc1ac6921550ca88546076ae8
Vlad <elphynkarelov@gmail.com>
2022-10-04 18:18:44 +0900

    learning javascript2

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 
-let countEl = document.getElementById("h1")
-
 let count = 0
+const countEL = document.getElementById("h1")
+const bigBtn = document.getElementById("big")
 
-function increment() {
+function increment(){
     count = count + 1
-    countEl.innerText = count
+    countEL.innerText = count
+
+}
+
+function save(){
+    console.log("Saved: ",countEL.innerText)
+}
+
+function del(){
+    count = 0
+    countEL.innerText = count
+    console.log("Reset count")
 }
